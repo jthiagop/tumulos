c4db81dffad794c09596128e0ea73e78a5661d73
José Thiago <jthiagopereira@gmail.com>
2025-07-02 13:48:54 -0300

    adicionar a rota de pagamento no botao

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -159,7 +159,7 @@
                                                     <!--begin:Menu item-->
                                                     <div class="menu-item p-0 m-0">
                                                         <!--begin:Menu link-->
-                                                        <a href="dashboards/ecommerce.html" class="menu-link">
+                                                        <a href="{{ route('pagamentos.index') }}" class="menu-link">
                                                             <span
                                                                 class="menu-custom-icon d-flex flex-center flex-shrink-0 rounded w-40px h-40px me-3">
                                                                 <i class="ki-outline ki-dollar text-info fs-1"></i>
